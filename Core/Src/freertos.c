6c1bfc1d886e2daa6d8eed09118ef76d41959b46
ayberkarslan <ayberkarslann0@gmail.com>
2026-04-05 17:09:49 +0300

    new branch

---
+++ Core/Src/freertos.c
@@ -0,0 +1,337 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * File Name          : freertos.c
+  * Description        : Code for freertos applications
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+// Uçuş state machine mantığı
+typedef enum {
+    WAITING_ON_PAD,        // Rampada fırlatma bekliyor
+    ON_FLIGHT,                // Roket fırlatıldı, irtifa artıyor
+    FALLING,               // Roket düşüşte
+    LANDING                 // Yere inildi, hareket bitti
+} FlightState;
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+// SİMÜLASYON VE UÇUŞ PARAMETRELERİ
+#define LAUNCH_ALTITUDE_REACHED	15.0f
+#define APOGEE_DROP_CONFIRM	5.0f
+#define MAIN_PARACHUTE_ALTITUDE	2000.0f
+#define DRAG_PARACHUTE_ALTITUDE	8000.0f
+#define LANDING_ALTITUDE_REACHED	10.0f
+
+#define G_FORCE_DELAY_MS  3000 //başta G kuvveti yüzünden bmp180'in verebileceği hatalı verilerden daha az etkilenmek amacıyla belirli bir süre low pass'daki
+
+#define LOW_PASS_ALPHA_NORMAL	0.2f    // low pass filtre katsayısı
+#define LOW_PASS_ALPHA_SECURE	0.05f
+
+
+#define NEGATIVE_VELOCITY_CONFIRM_VALUE	-2.0f
+#define LANDING_VELOCITY_CONFIRM_VALUE  0.5f
+// BMP180 sensörmüzn moduna göre maksimum okuma yapma sürelerini buldum ve bu sürelere göre bekleme yaptık döngü sonundaki HAL_Delay fonksiyonunda
+
+#define BMP180_MODE	3
+
+#if BMP180_MODE == 0
+#define SENSOR_WAIT_MS 10
+
+#elif BMP180_MODE == 1
+#define SENSOR_WAIT_MS 15
+
+#elif BMP180_MODE == 2
+#define SENSOR_WAIT_MS 25
+
+#elif BMP180_MODE == 3
+#define SENSOR_WAIT_MS 40
+
+#endif
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN Variables */
+FlightState currentState = WAITING_ON_PAD;
+
+float groundAltitude = 0.0f;     // Rampa kalibrasyon için iritifa değeri
+float currentRelativeAltitude = 0.0f; // Yere göre güncel filtrelenmiş irtifa
+float maxRelativeAltitude = 0.0f;     // Uçuş boyunca ulaşılan maksimum irtifa
+float previousAltitude = 0.0f;
+float verticalV = 0.0f;
+uint32_t lastVTime = 0.0f;
+
+// Low-Pass Filtre Değişkeni
+float filteredAltitude = 0.0f;
+uint32_t launchTime = 0;
+float currentAlpha = LOW_PASS_ALPHA_NORMAL;
+
+
+/* USER CODE END Variables */
+osThreadId defaultTaskHandle;
+osThreadId sensorOkuHandle;
+osThreadId drag_pHandle;
+osThreadId main_pHandle;
+osThreadId fsmHandle;
+osSemaphoreId dragSemHandle;
+osSemaphoreId mainSemHandle;
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN FunctionPrototypes */
+void SystemClock_Config(void);
+void MX_FREERTOS_Init(void);
+/* USER CODE BEGIN PFP */
+float getAltitude(int32_t pressure, float temperature);
+float lowpassfilter(float rawAltitude, float alpha);
+void launch_drag_parachute(){
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+}
+void launch_main_parachute(){
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);
+
+}
+
+void sendTelemetry(char* message);
+/* USER CODE END FunctionPrototypes */
+
+void StartDefaultTask(void const * argument);
+void sensorRead(void const * argument);
+void dragParachute(void const * argument);
+void mainParachute(void const * argument);
+void stateMachine(void const * argument);
+
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/* GetIdleTaskMemory prototype (linked to static allocation support) */
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize );
+
+/* USER CODE BEGIN GET_IDLE_TASK_MEMORY */
+static StaticTask_t xIdleTaskTCBBuffer;
+static StackType_t xIdleStack[configMINIMAL_STACK_SIZE];
+
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize )
+{
+  *ppxIdleTaskTCBBuffer = &xIdleTaskTCBBuffer;
+  *ppxIdleTaskStackBuffer = &xIdleStack[0];
+  *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+  /* place for user code */
+}
+/* USER CODE END GET_IDLE_TASK_MEMORY */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* Create the semaphores(s) */
+  /* definition and creation of dragSem */
+  osSemaphoreDef(dragSem);
+  dragSemHandle = osSemaphoreCreate(osSemaphore(dragSem), 1);
+
+  /* definition and creation of mainSem */
+  osSemaphoreDef(mainSem);
+  mainSemHandle = osSemaphoreCreate(osSemaphore(mainSem), 1);
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* definition and creation of defaultTask */
+  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
+  defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
+
+  /* definition and creation of sensorOku */
+  osThreadDef(sensorOku, sensorRead, osPriorityRealtime, 0, 128);
+  sensorOkuHandle = osThreadCreate(osThread(sensorOku), NULL);
+
+  /* definition and creation of drag_p */
+  osThreadDef(drag_p, dragParachute, osPriorityAboveNormal, 0, 128);
+  drag_pHandle = osThreadCreate(osThread(drag_p), NULL);
+
+  /* definition and creation of main_p */
+  osThreadDef(main_p, mainParachute, osPriorityAboveNormal, 0, 128);
+  main_pHandle = osThreadCreate(osThread(main_p), NULL);
+
+  /* definition and creation of fsm */
+  osThreadDef(fsm, stateMachine, osPriorityHigh, 0, 128);
+  fsmHandle = osThreadCreate(osThread(fsm), NULL);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+}
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void const * argument)
+{
+  /* USER CODE BEGIN StartDefaultTask */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartDefaultTask */
+}
+
+/* USER CODE BEGIN Header_sensorRead */
+/**
+* @brief Function implementing the sensorOku thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_sensorRead */
+void sensorRead(void const * argument)
+{
+  /* USER CODE BEGIN sensorRead */
+  /* Infinite loop */
+  for(;;)
+  {
+
+
+
+    osDelay(1);
+  }
+  /* USER CODE END sensorRead */
+}
+
+/* USER CODE BEGIN Header_dragParachute */
+/**
+* @brief Function implementing the drag_p thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_dragParachute */
+void dragParachute(void const * argument)
+{
+  /* USER CODE BEGIN dragParachute */
+  /* Infinite loop */
+  for(;;)
+  {
+
+
+
+    osDelay(1);
+  }
+  /* USER CODE END dragParachute */
+}
+
+/* USER CODE BEGIN Header_mainParachute */
+/**
+* @brief Function implementing the main_p thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_mainParachute */
+void mainParachute(void const * argument)
+{
+  /* USER CODE BEGIN mainParachute */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END mainParachute */
+}
+
+/* USER CODE BEGIN Header_stateMachine */
+/**
+* @brief Function implementing the fsm thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_stateMachine */
+void stateMachine(void const * argument)
+{
+  /* USER CODE BEGIN stateMachine */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END stateMachine */
+}
+
+/* Private application code --------------------------------------------------*/
+/* USER CODE BEGIN Application */
+// basınç ve sıcaklığı alarak daha keskin irtifa hesabı yaptık sadece basınç üzerinden hesap yapmaya kıyasla
+float getAltitude(int32_t pressure, float temperature) {
+
+
+
+	if(pressure <=0){
+		return filteredAltitude;
+	}
+	float T_kelvin = temperature + 273.15f;
+	    return ((pow((101325.0f / (float)pressure), 1.0f/5.257f) - 1.0f) * T_kelvin) / 0.0065f;
+}
+
+// Sensör Gürültüsünü Engellemek İçin Low-Pass Filtresi
+float lowpassfilter(float rawAltitude, float alpha){
+	filteredAltitude = (alpha*rawAltitude) + ((1.0f-alpha) * filteredAltitude);
+	return filteredAltitude;
+}
+
+
+void sendTelemetry(char* message) {
+    char buffer[200];
+    sprintf(buffer, "DURUM: %s\r\n"
+                    "Current Altitude: %.2f | Max Altitude: %.2f | State: %d | Temperature: %.2f\r\n",
+            message, currentRelativeAltitude, maxRelativeAltitude, (int)currentState, BMP180_GetTemperature());
+    HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), 100);
+}
+/* USER CODE END Application */
